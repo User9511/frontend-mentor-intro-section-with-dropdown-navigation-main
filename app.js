e56ebf48b9d644950310a181247d1a1fe4faac8e
Smgy94 <Shanemcgeown@gmail.com>
2023-01-13 22:53:51 +0000

    commit

--- app.js
+++ app.js
@@ -1,3 +1,35 @@
+// const menuIcon = document.querySelector(".menu-icon");
+// const closeIcon = document.querySelector(".menu-icon-close");
+// const nav = document.querySelector("nav");
+// const dropdownNav = document.querySelectorAll(".dropdown-nav");
+
+// // Add a click event listener to the menu icon
+// menuIcon.addEventListener("click", function () {
+//   // Toggle the display of the menu icon
+//   menuIcon.style.display = "none";
+//   // Toggle the display of the close icon
+//   closeIcon.style.display = "block";
+//   // Toggle the display of the nav
+//   nav.style.display = "block";
+// });
+
+// // Add a click event listener to the close icon
+// closeIcon.addEventListener("click", function () {
+//   // Toggle the display of the menu icon
+//   menuIcon.style.display = "block";
+//   // Toggle the display of the close icon
+//   closeIcon.style.display = "none";
+//   // Toggle the display of the nav
+//   nav.style.display = "none";
+// });
+
+// dropdownNav.forEach((item) => {
+//   item.addEventListener("click", function () {
+//     const dropdownList = this.querySelector(".dropdown-list");
+//     dropdownList.classList.toggle("show-dropdown");
+//   });
+// });
+
 const menuIcon = document.querySelector(".menu-icon");
 const closeIcon = document.querySelector(".menu-icon-close");
 const nav = document.querySelector("nav");
@@ -28,4 +60,18 @@ dropdownNav.forEach((item) => {
     const dropdownList = this.querySelector(".dropdown-list");
     dropdownList.classList.toggle("show-dropdown");
   });
+});
+
+const featuresBtn = document.getElementById("features");
+const featuresArrow = featuresBtn.querySelector("img");
+
+const companyBtn = document.getElementById("company");
+const companyArrow = companyBtn.querySelector("img");
+
+featuresBtn.addEventListener("click", function () {
+  featuresArrow.classList.toggle("rotate");
+});
+
+companyBtn.addEventListener("click", function () {
+  companyArrow.classList.toggle("rotate");
 });
