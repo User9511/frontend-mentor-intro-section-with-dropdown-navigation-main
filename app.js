fd23d700a7cd97a0b3bd58dbe6fa18b83511200b
Smgy94 <Shanemcgeown@gmail.com>
2023-01-13 11:38:10 +0000

    commit

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
+// Select the menu icon and close icon elements
 const menuIcon = document.querySelector(".menu-icon");
-const menuIconClose = document.querySelector(".menu-icon-close");
+const closeIcon = document.querySelector(".menu-icon-close");
 const nav = document.querySelector("nav");
 
+// Add a click event listener to the menu icon
 menuIcon.addEventListener("click", function () {
+  // Toggle the display of the menu icon
   menuIcon.style.display = "none";
-  menuIconClose.style.display = "block";
+  // Toggle the display of the close icon
+  closeIcon.style.display = "block";
+  // Toggle the display of the nav
   nav.style.display = "none";
 });
 
-menuIconClose.addEventListener("click", function () {
-  menuIconClose.style.display = "none";
+// Add a click event listener to the close icon
+closeIcon.addEventListener("click", function () {
+  // Toggle the display of the menu icon
   menuIcon.style.display = "block";
+  // Toggle the display of the close icon
+  closeIcon.style.display = "none";
+  // Toggle the display of the nav
   nav.style.display = "block";
 });
