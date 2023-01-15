701eadedcbc6003fd649f49982b780618eb36c8b
Smgy94 <Shanemcgeown@gmail.com>
2023-01-15 22:36:23 +0000

    commit

--- app.js
+++ app.js
@@ -1,35 +1,4 @@
-// const menuIcon = document.querySelector(".menu-icon");
-// const closeIcon = document.querySelector(".menu-icon-close");
-// const nav = document.querySelector("nav");
-// const dropdownNav = document.querySelectorAll(".dropdown-nav");
-
-// // Add a click event listener to the menu icon
-// menuIcon.addEventListener("click", function () {
-//   // Toggle the display of the menu icon
-//   menuIcon.style.display = "none";
-//   // Toggle the display of the close icon
-//   closeIcon.style.display = "block";
-//   // Toggle the display of the nav
-//   nav.style.display = "block";
-// });
-
-// // Add a click event listener to the close icon
-// closeIcon.addEventListener("click", function () {
-//   // Toggle the display of the menu icon
-//   menuIcon.style.display = "block";
-//   // Toggle the display of the close icon
-//   closeIcon.style.display = "none";
-//   // Toggle the display of the nav
-//   nav.style.display = "none";
-// });
-
-// dropdownNav.forEach((item) => {
-//   item.addEventListener("click", function () {
-//     const dropdownList = this.querySelector(".dropdown-list");
-//     dropdownList.classList.toggle("show-dropdown");
-//   });
-// });
-
+// TOGGLES THE MOBILE NAV MENU ICON OPEN/CLOSE§
 const menuIcon = document.querySelector(".menu-icon");
 const closeIcon = document.querySelector(".menu-icon-close");
 const nav = document.querySelector("nav");
@@ -55,6 +24,7 @@ closeIcon.addEventListener("click", function () {
   nav.style.display = "none";
 });
 
+// TOGGLES THE DROPDOWN NAV MENUS
 dropdownNav.forEach((item) => {
   item.addEventListener("click", function () {
     const dropdownList = this.querySelector(".dropdown-list");
@@ -62,9 +32,9 @@ dropdownNav.forEach((item) => {
   });
 });
 
+// ROTATE THE DROPDOWN  MENU ICON
 const featuresBtn = document.getElementById("features");
 const featuresArrow = featuresBtn.querySelector("img");
-
 const companyBtn = document.getElementById("company");
 const companyArrow = companyBtn.querySelector("img");
 
