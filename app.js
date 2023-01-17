7fbaa976cef0b847fc77a81259711b47b714b726
Smgy94 <Shanemcgeown@gmail.com>
2023-01-17 15:01:37 +0000

    commit

--- app.js
+++ app.js
@@ -1,7 +1,48 @@
+// // // TOGGLES THE MOBILE NAV MENU ICON OPEN/CLOSE§
+// // const menuIcon = document.querySelector(".menu-icon");
+// // const closeIcon = document.querySelector(".menu-icon-close");
+// // const nav = document.querySelector("nav");
+// // const dropdownNav = document.querySelectorAll(".dropdown-nav");
+
+// // // Add a click event listener to the menu icon
+// // menuIcon.addEventListener("click", function () {
+// //   // Toggle the display of the menu icon
+// //   menuIcon.style.display = "none";
+// //   // Toggle the display of the close icon
+// //   closeIcon.style.display = "block";
+// //   // Toggle the display of the nav
+// //   nav.style.display = "block";
+// // });
+
+// // // Add a click event listener to the close icon
+// // closeIcon.addEventListener("click", function () {
+// //   // Toggle the display of the menu icon
+// //   menuIcon.style.display = "block";
+// //   // Toggle the display of the close icon
+// //   closeIcon.style.display = "none";
+// //   // Toggle the display of the nav
+// //   nav.style.display = "none";
+// // });
+
+// // // TOGGLES THE DROPDOWN NAV MENUS
+// // dropdownNav.forEach((item) => {
+// //   item.addEventListener("click", function () {
+// //     const dropdownList = this.querySelector(".dropdown-list");
+// //     dropdownList.classList.toggle("show-dropdown");
+// //   });
+// // });
+
+// // // TOGGLES THE ROTATE FOR THE DROPDOWN ARROW
+// // document.querySelectorAll(".dropdown-btn").forEach(function (button) {
+// //   button.addEventListener("click", function () {
+// //     button.classList.toggle("rotate");
+// //   });
+// // });
+
 // // TOGGLES THE MOBILE NAV MENU ICON OPEN/CLOSE§
 // const menuIcon = document.querySelector(".menu-icon");
 // const closeIcon = document.querySelector(".menu-icon-close");
-// const nav = document.querySelector("nav");
+// const navWrapper = document.querySelector(".nav-wrapper");
 // const dropdownNav = document.querySelectorAll(".dropdown-nav");
 
 // // Add a click event listener to the menu icon
@@ -11,7 +52,7 @@
 //   // Toggle the display of the close icon
 //   closeIcon.style.display = "block";
 //   // Toggle the display of the nav
-//   nav.style.display = "block";
+//   navWrapper.style.display = "block";
 // });
 
 // // Add a click event listener to the close icon
@@ -21,7 +62,7 @@
 //   // Toggle the display of the close icon
 //   closeIcon.style.display = "none";
 //   // Toggle the display of the nav
-//   nav.style.display = "none";
+//   navWrapper.style.display = "none";
 // });
 
 // // TOGGLES THE DROPDOWN NAV MENUS
@@ -39,7 +80,6 @@
 //   });
 // });
 
-// TOGGLES THE MOBILE NAV MENU ICON OPEN/CLOSE§
 const menuIcon = document.querySelector(".menu-icon");
 const closeIcon = document.querySelector(".menu-icon-close");
 const navWrapper = document.querySelector(".nav-wrapper");
@@ -53,6 +93,9 @@ menuIcon.addEventListener("click", function () {
   closeIcon.style.display = "block";
   // Toggle the display of the nav
   navWrapper.style.display = "block";
+  // Update aria-expanded attribute
+  menuIcon.setAttribute("aria-expanded", "true");
+  closeIcon.setAttribute("aria-expanded", "false");
 });
 
 // Add a click event listener to the close icon
@@ -63,6 +106,9 @@ closeIcon.addEventListener("click", function () {
   closeIcon.style.display = "none";
   // Toggle the display of the nav
   navWrapper.style.display = "none";
+  // Update aria-expanded attribute
+  menuIcon.setAttribute("aria-expanded", "false");
+  closeIcon.setAttribute("aria-expanded", "true");
 });
 
 // TOGGLES THE DROPDOWN NAV MENUS
@@ -79,16 +125,3 @@ document.querySelectorAll(".dropdown-btn").forEach(function (button) {
     button.classList.toggle("rotate");
   });
 });
-
-// TOGGLE ARIA EXPANDED
-function toggleAriaExpanded() {
-  let x = document.getElementById("menu-icon").getAttribute("aria-expanded");
-  let y = document.getElementById("menu-icon-close").getAttribute("aria-expanded");
-
-  if (x === "true") {
-    x = "false";
-  } else {
-    x = "true";
-  }
-  document.getElementById("menu-icon").setAttribute("aria-expanded", x);
-}
