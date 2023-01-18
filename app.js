a32359cdf79d2ba19f2af110944e24c57d30fe92
Smgy94 <Shanemcgeown@gmail.com>
2023-01-18 14:31:04 +0000

    commit

--- app.js
+++ app.js
@@ -1,45 +1,45 @@
-const menuIcon = document.querySelector(".menu-icon-open");
-const closeIcon = document.querySelector(".menu-icon-close");
+const mobileMenuTrigger = document.getElementById("mobileMenuTrigger");
 const navWrapper = document.getElementById("nav-wrapper");
-const dropdownNav = document.querySelectorAll(".dropdown-nav");
+const featuresDropdownTrigger = document.getElementById("featuresDropdownTrigger");
+const companyDropdownTrigger = document.getElementById("companyDropdownTrigger");
+const featuresList = document.getElementById("features-list");
+const companyList = document.getElementById("company-list");
 
-// Add a click event listener to the menu icon
-menuIcon.addEventListener("click", function () {
-  // Toggle the display of the menu icon
-  menuIcon.style.display = "none";
-  // Toggle the display of the close icon
-  closeIcon.style.display = "block";
-  // Toggle the display of the nav
-  navWrapper.style.display = "block";
-  // Update aria-expanded attribute
-  menuIcon.setAttribute("aria-expanded", "true");
-  closeIcon.setAttribute("aria-expanded", "false");
+// Add a click event listener to the mobile menu trigger button
+mobileMenuTrigger.addEventListener("click", () => {
+  // toggle the active class on the nav wrapper and mobile menu trigger button
+  navWrapper.classList.toggle("active");
+  mobileMenuTrigger.classList.toggle("active");
+
+  // toggle the aria-expanded attribute on the mobile menu trigger button
+  if (navWrapper.classList.contains("active")) {
+    mobileMenuTrigger.setAttribute("aria-expanded", "true");
+  } else {
+    mobileMenuTrigger.setAttribute("aria-expanded", "false");
+  }
 });
 
-// Add a click event listener to the close icon
-closeIcon.addEventListener("click", function () {
-  // Toggle the display of the menu icon
-  menuIcon.style.display = "block";
-  // Toggle the display of the close icon
-  closeIcon.style.display = "none";
-  // Toggle the display of the nav
-  navWrapper.style.display = "none";
-  // Update aria-expanded attribute
-  menuIcon.setAttribute("aria-expanded", "false");
-  closeIcon.setAttribute("aria-expanded", "true");
+// Add a click event listener to the features dropdown trigger button
+featuresDropdownTrigger.addEventListener("click", () => {
+  featuresList.classList.toggle("show-dropdown");
+
+  // toggle the aria-expanded attribute on the features dropdown trigger button
+  if (featuresList.classList.contains("show-dropdown")) {
+    featuresDropdownTrigger.setAttribute("aria-expanded", "true");
+  } else {
+    featuresDropdownTrigger.setAttribute("aria-expanded", "false");
+  }
 });
 
-// TOGGLES THE DROPDOWN NAV MENUS
-dropdownNav.forEach((item) => {
-  item.addEventListener("click", function () {
-    const dropdownList = this.querySelector(".dropdown-list");
-    dropdownList.classList.toggle("show-dropdown");
-  });
-});
+// Add a click event listener to the company dropdown trigger button
+companyDropdownTrigger.addEventListener("click", () => {
+  companyList.classList.toggle("show-dropdown");
 
-// TOGGLES THE ROTATE FOR THE DROPDOWN ARROW
-document.querySelectorAll(".dropdown-btn").forEach(function (button) {
-  button.addEventListener("click", function () {
-    button.classList.toggle("rotate");
-  });
+  // toggle the aria-expanded attribute on the company dropdown trigger button
+  if (companyList.classList.contains("show-dropdown")) {
+    companyDropdownTrigger.setAttribute("aria-expanded", "true");
+  } else {
+    companyDropdownTrigger.setAttribute("aria-expanded", "false");
+  }
 });
+``;
